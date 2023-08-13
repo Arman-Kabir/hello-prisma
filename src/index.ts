b17d3460e9975f444f4dfe0d2253994bb3668602
Arman-Kabir <armankabir8@gmail.com>
2023-08-13 18:40:43 +0600

    basic express setup done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import express, { Application } from "express";
+import cors from 'cors';
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 
 const prisma = new PrismaClient();
+const port = process.env.PORT || 3003;
 
-async function main(){
 
+async function main() {
+    app.listen(port, () => {
+        
+    })
     // const getAllUsers = await prisma.user.findMany();
     // console.log(getAllUsers);
     // const postUser = await prisma.user.create({
@@ -14,7 +25,7 @@ async function main(){
     //     }
     // });
     // console.log(postUser);
-    
+
 }
 
 main();
